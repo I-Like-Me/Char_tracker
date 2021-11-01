4e63c6c1b10e28dde1ae518d797ea5f8b865fffe
Cass Golkin <cass.golkin@AD.SHC.SA.NYU.EDU>
2021-11-01 15:31:57 -0400

    Added mover and finder to char class

--- char_builder.py
+++ char_builder.py
@@ -1,4 +1,19 @@
 class CharSpawn:
 
     def __init__(self, name, x_pos=0, y_pos=0):
-        pass
+        self.name = name
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        print(f"{self.name} starting at {self.x_pos},{self.y_pos}")
+
+    def move_char(self, new_x, new_y):
+        print(f"{self.name} moved from {self.x_pos},{self.y_pos} to {new_x},{new_y}")
+        self.x_pos = new_x
+        self.y_pos = new_y
+
+    def where_am_i(self):
+        print(f"{self.name} currently at {self.x_pos},{self.y_pos}")
+
+kevin = CharSpawn("Kevin", 3, 7)
+kevin.move_char(5,2)
+kevin.where_am_i()
