990fa5fd5d085303ba5af1ce451e7f7748c64c2a
Cass Golkin <cass.golkin@AD.SHC.SA.NYU.EDU>
2021-11-02 16:59:22 -0400

    Printable Char

--- char_tracker.py
+++ char_tracker.py
@@ -1,8 +1,10 @@
 from char_builder import CharSpawn
 
-def stats_check(map_size=0, char_list=None):
+def stats_check(map_size=0, char_list=[]):
     char_1_name = input("Who are you: ")
     x, y = input("Where would you like to start: ").split()
     player_1 = CharSpawn(char_1_name, x, y)
+    char_list.append(player_1)
+    print(char_list)
 
 stats_check()
